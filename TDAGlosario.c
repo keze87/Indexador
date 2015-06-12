205ca4b23b022b1931708efed4ac1ea7066323e1
Karina <kari-gisela@users.noreply.github.com>
2015-06-12 10:31:36 -0300

    Funcion guardar_ordenado
    guarda en el arbol las palabras de forma ordenada

--- TDAGlosario.c
+++ TDAGlosario.c
@@ -24,6 +24,36 @@ int CrearGlosario(TDAGlosario* g, char* documento, char* arch_config)
 
 }
 
+int guardar_ordenado(TAB *arbol, TPalabra elem){    /*esta hecho recursivo*/
+    int error=OK;
+    int mov=OK;
+    TPalabra aux;
+
+    if (AB_Vacio(*arbol)){ /*si el arbol esta vacio, lo inserto en el raiz*/
+        error=AB_Insertar(arbol, RAIZ, &elem);
+        return error;
+    }
+
+    AB_ElemCte(*arbol, &aux);
+
+    if(strcmp(elem.palabra, aux.palabra)<0){
+        error=AB_MoverCte(arbol, IZQ);
+        if (error==FALSE) /*si no se pudo mover es porque no tiene un hijo IZQ. entonces inserto a la izquierda*/
+            return AB_Insertar(arbol, IZQ, &elem);
+        else
+            return(guardar_ordenado(arbol, elem));
+    }
+
+    if(strcmp(elem.palabra, aux.palabra)>0){
+        error=AB_MoverCte(arbol, DER);
+        if (error==FALSE) /*si no se pudo mover es porque no tiene un hijo DER. entonces inserto a la derecha*/
+            return AB_Insertar(arbol, DER, &elem);
+        else
+            return(guardar_ordenado(arbol, elem));
+    }
+
+}
+
 int DestruirGlosario(TDAGlosario* g)
 {
 
