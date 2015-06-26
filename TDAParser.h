ea849812ee51e6f914ea1cb12a9071c614b2a289
Karina <kari-gisela@users.noreply.github.com>
2015-06-26 10:38:38 -0300

    Arreglo de las primitivas 2

--- TDAParser.h
+++ TDAParser.h
@@ -15,15 +15,23 @@ typedef struct TDAParser{
 	TListaSimple parser;
 }TDAParser;
 
+/*
+PRE: el archivo de configuración debe tener el formato pedido en el enunciado del TP
+POST: crea el TDAParser con las palabras del texto y sus posiciones
+*/
 int PA_Crear(char* RutaDoc, char* RutaConf, TDAParser* Resultado);
 
+/*
+PRE: Parser creado
+POST: Destruye la estructura del TDA
+*/
 int PA_Destruir(TDAParser* Parser);
 
+/*
+PRE: ListaParser creada, el archivo de RutaDoc debe ser válido.
+POST: Inserta en ListaParser las palabras con su posicion en el archivo <RutaDoc>.
+*/
 int PA_SigPalabra(char* RutaDoc, int cant_separadores, char* separadores, TListaSimple* ListaParser);
 
-/* Procesa archivo Configraciones */
-int PConfiguraciones(char* RutaConf, char separadores[255]);
-
-int insertar_lista(TListaSimple* ListaParser, TElemParser Elem);
 
 #endif
