64d8762c488d4013914d61a048be3e5a89790b48
Karina <kari-gisela@users.noreply.github.com>
2015-06-26 10:12:56 -0300

    Arreglo de las primitivas

--- TDAGlosario.h
+++ TDAGlosario.h
@@ -25,20 +25,30 @@ typedef struct TPosicion{
 	int pos;
 }TPosicion;
 
+/*
+PRE: arch_config debe tener el formato enunciado en el TP.
+POST: crea la estructura parseando documento, teniendo en
+cuenta el arch_config para parsearlo y deja lista la estructura para su explotación.
+*/
 int CrearGlosario(TDAGlosario* g, char* documento, char* arch_config);
 
+/*
+PRE: g creado
+POST: elimina la información de la estructura
+*/
 int DestruirGlosario(TDAGlosario* g);
 
+/*
+PRE: LResultado lista simple creada y vacía, g creado.
+POST: devuelve en LResultado la lista con las posiciones en que fue encontrada la palabra
+*/
 int ConsultarPalabraGlosario(TDAGlosario* g, char* palabra, TListaSimple* LResultado);
 
+/*
+PRE: Lresultado creada y vacía, g creado.
+POST: Devuelve en LResultado el ranking de las palabras ordenado de forma descendente por cantidad de apariciones.
+*/
 int Ranking_palabras_Glosario(TDAGlosario* g, TListaSimple* LResultado);
 
-int lista_a_arbol(TDAGlosario* g, TDAParser ListaParser);
-
-int guardar_ordenado(TAB *arbol, TPalabra elem);
-
-void DestruirListas(TAB arbol, int mov);
-
-int busqueda(TAB *arbol, char *palabra);
 
 #endif
